5d1eb903a8d36d6be4726521bbd433cde90580c0
hamouda142 <100885729+hamouda142@users.noreply.github.com>
2025-08-06 05:13:30 +0200

    Add files via upload

---
+++ SimuPump_types.h
@@ -0,0 +1,32 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: SimuPump_types.h
+ *
+ * Code generated for Simulink model 'SimuPump'.
+ *
+ * Model version                  : 1.13
+ * Simulink Coder version         : 25.1 (R2025a) 21-Nov-2024
+ * C/C++ source code generated on : Wed Aug  6 04:33:46 2025
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef SimuPump_types_h_
+#define SimuPump_types_h_
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_SimuPump_T RT_MODEL_SimuPump_T;
+
+#endif                                 /* SimuPump_types_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
